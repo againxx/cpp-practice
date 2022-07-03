a089b0f44e27eb5885c10d601fc5609420d6a2fe
Xi Xia <againxx@mail.ustc.edu.cn>
2022-07-03 10:47:16 +0800

    Add c_syscall_learning

---
+++ c_syscall_learning/read_write.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int getchar_v1(void) {
+    char c;
+    return (read(0, &c, 1) == 1) ? (unsigned char)c : EOF;
+}
+
+int getchar_v2(void) {
+    static char buf[BUFSIZ];
+    static char *bufp = buf;
+    static int n = 0;
+
+    if (n == 0) {
+        n = read(0, buf, sizeof buf);
+        bufp = buf;
+    }
+    return (--n >= 0) ? (unsigned char) *bufp++ : EOF;
+}
+
+int main() {
+    char buf[BUFSIZ];
+    int n;
+
+    while ((n = read(0, buf, BUFSIZ)) > 0) {
+        write(1, buf, n);
+    }
+    return 0;
+}
